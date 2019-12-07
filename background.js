35b9200a5e1268108e61d067ffc04ba5d1e9a849
Albert <albert.khm@gmail.com>
2019-12-07 22:56:48 +0300

    keyboard shortcuts changed, mousewheel event added in content script, code cleaned

--- background.js
+++ background.js
@@ -1,36 +1,6 @@
 'use strict';
 
 let columnWidthTabs = [];
-let curBodyWidth = null;
-
-
-chrome.runtime.onInstalled.addListener(function () {
-
-    /*    chrome.storage.sync.set({color: '#3aa757'}, function () {
-            console.log('The color is green.');
-        });
-
-        chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-            chrome.declarativeContent.onPageChanged.addRules([{
-                conditions: [new chrome.declarativeContent.PageStateMatcher({
-                    pageUrl: {hostEquals: 'developer.chrome.com'},
-                })],
-                actions: [new chrome.declarativeContent.ShowPageAction()]
-            }]);
-        });*/
-
-
-});
-
-
-//////////////////////////////
-
-/*chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: `body.style.margin = '0 auto'`});
-});*/
-
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     //code in here will run every time a user goes onto a new tab, so you can insert your scripts into every new tab
@@ -53,11 +23,11 @@ chrome.commands.onCommand.addListener(function (command) {
             registeredTab = tabs[tabs.length - 1];
         }
 
-        if (command === 'godown') {
+        if (command === 'up') {
             if (registeredTab.width < 100) {
                 registeredTab.width += 5;
             }
-        } else if (command === 'godown222') {
+        } else if (command === 'down') {
             if (registeredTab.width > 0) {
                 registeredTab.width -= 5;
             }
@@ -70,35 +40,9 @@ chrome.commands.onCommand.addListener(function (command) {
 
 });
 
-function _getCurrentTab(callback) { //Take a callback
-    chrome.tabs.query({active: true, currentWindow: true}, function (tab) {
-        callback(tab); //call the callback with argument
-    });
-}
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    console.log(request);
+    sendResponse();
+});
 
-function _displayTab(tab) { //define your callback function
-    console.log(tab);
-
-    chrome.tabs.executeScript(
-        tab.id,
-        {code: `document.body.style.width = ${curBodyWidth}%`}
-    );
-
-}
-
-//_getCurrentTab(_displayTab); //invoke the function with the callback function reference
-
-
-function setBodyWidth(value) {
-    /*
-    chrome.tabs.query({active: true, currentWindow: true}, function (tab) {
-            chrome.tabs.executeScript(
-                tab.id,
-                {code: `document.body.style.width = ${value}%`}
-                );
-        });
-
-        */
-    _getCurrentTab(_displayTab)
-}
 
--- contentScript.js
+++ contentScript.js
@@ -6,12 +6,36 @@ console.log('content scr');
 //document.body.style.marginRight = 'auto';
 document.body.style.margin = '0 auto';
 
+if (document.addEventListener) {
+    document.addEventListener("mousewheel", wheelHandler, false);
+}
+
+function wheelHandler(event) {
+    if (event.shiftKey && event.altKey) {
+        let command = null;
+        if (event.deltaY < 0) {
+            command = 'up';
+        } else {
+            command = 'down';
+        }
+        //console.log(command);
+
+        chrome.runtime.sendMessage({
+            command: command
+        });
+    }
+}
+
+function setPageWidth(value) {
+    document.body.style.width = `${value}%`;
+}
+
 console.log('add listener ahead');
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
+    function (request, sender, sendResponse) {
         console.log(request.width);
-        document.body.style.width =`${request.width}%`;
-        console.log()
+        setPageWidth(request.width);
+        sendResponse('response');
     }
 );
 
